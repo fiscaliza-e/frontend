3ecb270f307a42ca6cb93c85f88bae0d590ac932
Alice <ajsc1@aluno.ifal.edu.br>
2025-05-28 21:07:21 -0300

    criando componente footer e alterando layout.tsx

---
+++ src/components/footer/index.tsx
@@ -0,0 +1,13 @@
+import styles from "./footer.module.css";
+
+export default function Footer() {
+  return (
+    <footer className={styles.footerConteiner}>
+      <div className={styles.footerAnchorContainer}>
+        <a href="">Termos de Uso</a>
+        <a href="">FAQ</a>
+      </div>
+      <p className={styles.footerAllRight}>© 2025 FISCALIZA-e — Todos os direitos reservados.</p>
+    </footer>
+  );
+}
