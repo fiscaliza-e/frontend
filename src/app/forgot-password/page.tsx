967f06ee46f32e99be6b1d6bc6012b6952da2825
Alice <ajsc1@aluno.ifal.edu.br>
2025-06-06 20:16:44 -0300

    add component Card

--- src/app/forgot-password/page.tsx
+++ src/app/forgot-password/page.tsx
@@ -1,43 +1,44 @@
 "use client";
-import { UserOutlined } from "@ant-design/icons";
-import { Button, Checkbox, Form, Input, Flex } from "antd";
+import { Button, Form, Input, Flex } from "antd";
 
 import styles from "./forgot-password.module.css";
+import Card from "@/components/card";
+import classNames from "classnames";
 
 export default function ForgotPassword() {
   return (
-    <Form
-      className={styles.formLayout}
-      name="forgotPassword"
-      initialValues={{ remember: true }}
-      style={{ minWidth: 300 }}
-      onFinish={() => {}}
-    >
-      <Flex vertical style={{ minWidth: 360 }}>
-        <h3 className={styles.formTitle}>Redefinir Senha</h3>
-        <p>
-          Para recuperar sua senha, insira o e-mail informado ao se cadastrar.
-        </p>
-      </Flex>
-
-      <Form.Item
-        name="email"
-        style={{ margin: "1rem 0" }}
-        rules={[
-          { required: true, message: "Por favor, informe um email valido!" },
-        ]}
+    <Card title="Redefinir Senha">
+      <Form
+        name="forgotPassword"
+        initialValues={{ remember: true }}
+        style={{ minWidth: 300 }}
+        onFinish={() => {}}
       >
-        <Input prefix={""} placeholder="email@example.com" />
-      </Form.Item>
-      <Flex justify="center">
-        <Button
-          className={[styles.submitButton, styles.hoverButton].join(" ")}
-          type="primary"
-          htmlType="submit"
+        <Flex vertical style={{ minWidth: 360 }}>
+          <p>
+            Para recuperar sua senha, insira o e-mail informado ao se cadastrar.
+          </p>
+        </Flex>
+
+        <Form.Item
+          name="email"
+          style={{ marginTop: "1rem" }}
+          rules={[
+            { required: true, message: "Por favor, informe um email valido!" },
+          ]}
         >
-          Enviar
-        </Button>
-      </Flex>
-    </Form>
+          <Input placeholder="email@example.com" />
+        </Form.Item>
+        <Flex justify="center">
+          <Button
+            className={classNames(styles.submitButton, styles.hoverButton)}
+            type="primary"
+            htmlType="submit"
+          >
+            Enviar
+          </Button>
+        </Flex>
+      </Form>
+    </Card>
   );
 }
