90c3fc9a8b5ae04acb72badafaef33905a9d67bf
Alice <ajsc1@aluno.ifal.edu.br>
2025-06-12 15:22:08 -0300

    Add mocks para desenvolvimento do front, página de visualizar reclamações com filtro de busca

---
+++ src/app/user/my-complaints/components/filter-complaints/filter-complaints.tsx
@@ -0,0 +1,33 @@
+import { Flex } from "antd";
+import styles from "./filter-complaints.module.css";
+
+import options from "@/mock/filter-options/index";
+
+interface Props {
+  onChange: (value: string) => void;
+}
+
+export default function FilterComplaints({ onChange }: Props) {
+  return (
+    <Flex vertical justify="center" className={styles.filters}>
+      <Flex justify="space-between" className={styles.filterComplaints}>
+        {options.map((option, index) => (
+          <Flex>
+            <input
+              id={option.id}
+              name="filters"
+              type="radio"
+              className={styles.filterRadio}
+              value={option.id}
+              defaultChecked={index === 0}
+              onChange={(e) => onChange(e.target.value)}
+            />
+            <label htmlFor={option.id} className={styles.filterLabel}>
+              {option.label}
+            </label>
+          </Flex>
+        ))}
+      </Flex>
+    </Flex>
+  );
+}
---
+++ src/mock/users/index.ts
@@ -0,0 +1,8 @@
+export const users = [
+  {
+    cpf: "123.456.789-00",
+    name: "Alice Julia Silva Costa",
+    email: "alice.costa@email.com",
+    password: "senhaSuperSecreta123",
+  },
+];
